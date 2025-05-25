2f6d757985cb68419106f31b5da07bb4e0020b82
Craig Thacker <craigthackerx@gmail.com>
2025-05-25 14:39:30 +0100

    Add destroy

--- terraform/allstackskip-99_azdo-pipelines-setup/jobs.tf
+++ terraform/allstackskip-99_azdo-pipelines-setup/jobs.tf
@@ -57,3 +57,23 @@ resource "azuredevops_build_definition" "jobs_init_plan_destroy" {
     service_connection_id = data.azuredevops_serviceendpoint_github.github.id
   }
 }
+
+resource "azuredevops_build_definition" "jobs_init_plan_destroy_apply" {
+  for_each        = local.envs
+  project_id      = data.azuredevops_project.target.id
+  name            = "${title(local.repo_name)} - Jobs - ${title(each.key)} - Terraform Init, Terraform Plan Destroy & Terraform Destroy"
+  path            = "${local.folders_path[each.key]}\\jobs"
+  agent_pool_name = "Default"
+
+  ci_trigger {
+    use_yaml = false
+  }
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.github_org_name}/${var.github_project_name}"
+    branch_name           = local.default_branch
+    yml_path              = ".azuredevops/workflows/jobs/${each.key}/terraform-init-plan-destroy-apply.yaml"
+    service_connection_id = data.azuredevops_serviceendpoint_github.github.id
+  }
+}
